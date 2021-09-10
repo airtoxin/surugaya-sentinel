c41054739dced28ba5bd0a44aa1aa54df7fb55a4
airtoxin <airtoxin@users.noreply.github.com>
2021-09-10 22:40:19 +0000

    deploy: 6d02b638308d419ba6b3734257799083aaafa5c8

---
+++ _next/static/chunks/pages/index-528a28bcb735392affa0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{6057:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(4379)}])}},function(n){n.O(0,[67,379,888,179],(function(){return _=6057,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ _next/static/chunks/pages/result/[timestamp]-8a4558340b2b06ac5c05.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[858],{8452:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/result/[timestamp]",function(){return u(8616)}])}},function(n){n.O(0,[67,616,888,179],(function(){return _=8452,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
